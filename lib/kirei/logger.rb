b9e9a31f9ffc442f6a0ccf8d70050cb779b213d8
Lud <lud@reinmiedl.com>
2023-09-11 21:41:34 +0200

    adds a base logger

---
+++ lib/kirei/logger.rb
@@ -0,0 +1,176 @@
+# typed: strict
+# frozen_string_literal: true
+
+module Kirei
+  # rubocop:disable Metrics
+
+  #
+  # Example Usage:
+  #
+  #    LogHandler.call(
+  #      level: :info,
+  #      label: "Request started",
+  #      meta: {
+  #        key: "value",
+  #      },
+  #    )
+  #
+  # You can define a custom log transformer to transform the logline:
+  #
+  #    Kirei.config.log_transformer = Proc.new { _1 }
+  #
+  # By default, "meta" is flattend and sensitive values are masked using see `Kirei.config.sensitive_keys`.
+  # You can also build on top the provided log transformer:
+  #
+  #   Kirei.config.log_transformer = Proc.new do |meta|
+  #      flattened_meta = Kirei::Logger.flatten_hash_and_mask_sensitive_values(meta)
+  #      # do something with the flattened meta
+  #      flattened_meta.map { _1.to_json }
+  #   end
+  #
+  # NOTE: the log transformer must return an array of strings to allow emitting multiple lines per log event.
+  #
+  class Logger
+    extend T::Sig
+
+    FILTERED = "[FILTERED]"
+
+    @instance = T.let(nil, T.nilable(Kirei::Logger))
+
+    sig { void }
+    def initialize
+      @queue = T.let(Thread::Queue.new, Thread::Queue)
+      @thread = T.let(start_logging_thread, Thread)
+    end
+
+    sig { returns(Kirei::Logger) }
+    def self.instance
+      @instance ||= new
+    end
+
+    sig { returns(::Logger) }
+    def self.logger
+      return @logger unless @logger.nil?
+
+      @logger = T.let(nil, T.nilable(::Logger))
+      @logger ||= ::Logger.new($stdout)
+
+      # we want the logline to be parseable to JSON
+      @logger.formatter = proc do |_severity, _datetime, _progname, msg|
+        "#{msg}\n"
+      end
+
+      @logger
+    end
+
+    sig do
+      params(
+        level: T.any(String, Symbol),
+        label: String,
+        meta: T::Hash[Symbol, T.untyped],
+      ).void
+    end
+    def self.call(level:, label:, meta: {})
+      return if ENV["LOGGER"] == "disabled"
+
+      instance.call(level: level, label: label, meta: meta)
+    end
+
+    sig do
+      params(
+        level: T.any(String, Symbol),
+        label: String,
+        meta: T::Hash[Symbol, T.untyped],
+      ).void
+    end
+    def call(level:, label:, meta: {})
+      meta[:"service.instance.id"] = Thread.current[:request_id]
+
+      # The Ruby logger only accepts one string as the only argument
+      @queue << { level: level, label: label, meta: meta }
+    end
+
+    sig { returns(Thread) }
+    def start_logging_thread
+      Thread.new do
+        Kernel.loop do
+          log_data = T.let(@queue.pop, T::Hash[Symbol, T.untyped])
+          level = log_data.fetch(:level)
+          label = log_data.fetch(:label)
+          meta = T.let(log_data.fetch(:meta), T::Hash[Symbol, T.untyped])
+          meta[:"service.version"] = Kirei.version
+          meta[:timestamp] = Time.current.utc.iso8601
+          meta[:level] = level.to_s.upcase
+          meta[:label] = label
+
+          log_transformer = Kirei.config.log_transformer
+
+          loglines = if log_transformer
+            log_transformer.call(meta)
+          else
+            [Oj.dump(Kirei::Logger.flatten_hash_and_mask_sensitive_values(meta))]
+          end
+
+          loglines.each { Kirei::Logger.logger.error(_1) }
+        end
+      end
+    end
+
+    # rubocop:disable Naming/MethodParameterName
+    sig do
+      params(
+        k: Symbol,
+        v: String,
+      ).returns(String)
+    end
+    def self.mask(k, v)
+      return Kirei::Logger::FILTERED if Kirei.config.sensitive_keys.any? { k.match?(_1) }
+
+      v
+    end
+    # rubocop:enable Naming/MethodParameterName
+
+    sig do
+      params(
+        hash: T::Hash[Symbol, T.untyped],
+        prefix: Symbol,
+      ).returns(T::Hash[Symbol, T.untyped])
+    end
+    def self.flatten_hash_and_mask_sensitive_values(hash, prefix = :'')
+      result = T.let({}, T::Hash[Symbol, T.untyped])
+      hash.deep_symbolize_keys!
+
+      hash.each do |key, value|
+        new_prefix = prefix.blank? ? key : :"#{prefix}.#{key}"
+
+        case value
+        when Hash then result.merge!(flatten_hash_and_mask_sensitive_values(value.symbolize_keys, new_prefix))
+        when Array
+          value.each_with_index do |element, index|
+            if element.is_a?(Hash) || element.is_a?(Array)
+              result.merge!(flatten_hash_and_mask_sensitive_values({ index => element }, new_prefix))
+            else
+              result[:"#{new_prefix}.#{index}"] = element.is_a?(String) ? mask(key, element) : element
+            end
+          end
+        when String then result[new_prefix] = mask(key, value)
+        when Numeric, FalseClass, TrueClass, NilClass then result[new_prefix] = value
+        else
+          if value.respond_to?(:serialize)
+            serialized_value = value.serialize
+            if serialized_value.is_a?(Hash)
+              result.merge!(flatten_hash_and_mask_sensitive_values(serialized_value.symbolize_keys, new_prefix))
+            else
+              result[new_prefix] = serialized_value&.to_s
+            end
+          else
+            result[new_prefix] = value&.to_s
+          end
+        end
+      end
+
+      result
+    end
+  end
+  # rubocop:enable Metrics
+end
