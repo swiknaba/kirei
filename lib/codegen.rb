da8a410d2b88d2989f2fb040ad9384574fdce3f2
Lud <lud@reinmiedl.com>
2023-09-23 16:30:21 +0200

    autogenerate relation klasses

---
+++ lib/codegen.rb
@@ -0,0 +1,20 @@
+# typed: strict
+# frozen_string_literal: true
+
+# this must run **after** Kirei has booted and been initialized
+# auto generates klasses dynamically. See lib/kirei/templates/**/*.rb
+
+#
+# Generates "Relations::XXRelation" klasses for app/models
+#
+Dir[
+  File.join(Kirei.root, "app/models/**/*.rb"),
+].each do |model_file|
+  require(model_file)
+
+  full_klass_name = T.must(model_file[%r{.*/app/(.*)\.rb}, 1]).classify
+  klass_name = T.let(File.basename(model_file, ".rb").camelize, String)
+  table_name = full_klass_name.constantize.table_name # rubocop:disable Sorbet/ConstantsFromStrings
+
+  eval(Templates::RelationKlass.erb(klass_name, table_name)) # rubocop:disable Security/Eval
+end
