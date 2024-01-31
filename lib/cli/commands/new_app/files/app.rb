a3a09b123f46334322ed8f029758a5dc82a06a90
Lud <lud@reinmiedl.com>
2024-01-31 22:53:51 +0100

    load files in correct order

--- lib/cli/commands/new_app/files/app.rb
+++ lib/cli/commands/new_app/files/app.rb
@@ -14,6 +14,24 @@ module Cli
               # typed: true
               # frozen_string_literal: true
 
+              # First: check if all gems are installed correctly
+              require 'bundler/setup'
+
+              # Second: load all gems
+              #         we have runtime/production ("default") and development gems ("development")
+              Bundler.require(:default)
+              Bundler.require(:development) if ENV['RACK_ENV'] == 'development'
+              Bundler.require(:test) if ENV['RACK_ENV'] == 'test'
+
+              # Third: load all initializers
+              Dir[File.join(__dir__, 'config/initializers', '*.rb')].each { require(_1) }
+
+              # Fourth: load all application code
+              Dir[File.join(__dir__, 'app/**/*', '*.rb')].each { require(_1) }
+
+              # Fifth: load configs
+              Dir[File.join(__dir__, 'config', '*.rb')].each { require(_1) }
+
               class #{app_name} < Kirei::Base
               end
             RUBY
