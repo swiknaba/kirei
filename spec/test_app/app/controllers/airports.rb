a301e8c90298888ddf7b1f0ebff88dc2ae21e785
Lud <lud@reinmiedl.com>
2024-03-09 13:53:35 +0100

    test app: add airports controller

---
+++ spec/test_app/app/controllers/airports.rb
@@ -0,0 +1,19 @@
+# typed: strict
+# frozen_string_literal: true
+
+module Controllers
+  class Airports < Kirei::BaseController
+    extend T::Sig
+
+    sig { returns(Kirei::Middleware::RackResponseType) }
+    def index
+      airports = ::Airport.all
+      data = Oj.dump(airports.map(&:serialize))
+
+      render(
+        status: 200,
+        body: data,
+      )
+    end
+  end
+end
