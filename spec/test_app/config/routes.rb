b48a9232d49f587210041dfb432b493ce4e54723
Lud <lud@reinmiedl.com>
2024-03-09 13:37:07 +0100

    clean up router gen

--- spec/test_app/config/routes.rb
+++ spec/test_app/config/routes.rb
@@ -1,16 +1,13 @@
 # typed: strict
 # frozen_string_literal: true
 
-# Define routes as follows:
-#
-# Kirei::Router::Route.new(
-#   method: 'GET',
-#   path: '/livez',
-#   controller: Controllers::HealthController,
-#   action: 'livez',
-# )
-#
 Kirei::Router.add_routes([
+  # Route.new(
+  #   method: "GET",
+  #   path: "/livez",
+  #   controller: Controllers::HealthController,
+  #   action: "livez",
+  # ),
   Kirei::Router::Route.new(
     method: "GET",
     path: "/airports",
