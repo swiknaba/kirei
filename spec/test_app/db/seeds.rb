755dd8aad9f31536e7508f5b7bbff97fa646c83d
Lud <lud@reinmiedl.com>
2024-05-11 16:03:29 +0200

    idempotent seed script

--- spec/test_app/db/seeds.rb
+++ spec/test_app/db/seeds.rb
@@ -1,5 +1,19 @@
+Airport.db.delete
+
 muc = Airport.new(
   id: 'MUC',
   name: 'Munich Airport'
 )
 muc.save
+
+ber = Airport.new(
+  id: 'BER',
+  name: 'Berlin Brandenburg Airport'
+)
+ber.save
+
+sfo = Airport.new(
+  id: 'SFO',
+  name: 'San Francisco International Airport'
+)
+sfo.save
