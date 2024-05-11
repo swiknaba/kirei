fef1bed15c4003b1b45fc7f1a573241f4ef74edf
Lud <lud@reinmiedl.com>
2024-05-11 17:40:00 +0200

    test annotation script on test app models

--- spec/test_app/app/models/airport.rb
+++ spec/test_app/app/models/airport.rb
@@ -1,6 +1,14 @@
 # typed: strict
 # frozen_string_literal: true
 
+# == Schema Info
+#
+# Table name: airports
+#
+#  id                  :text    not null, primary key
+#  name                :text    not null
+#
+
 class Airport < T::Struct
   extend T::Sig
   include Kirei::BaseModel
