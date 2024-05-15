956cb92526ed0acaf0fa5330cc5b7392b80242fa
Lud <lud@reinmiedl.com>
2024-05-15 17:36:28 +0800

    Update json_api_error.rb

---
+++ lib/kirei/errors/json_api_error.rb
@@ -0,0 +1,19 @@
+# typed: strict
+# frozen_string_literal: true
+
+module Kirei
+    module Errors
+      # https://jsonapi.org/format/#errors
+      # Error objects MUST be returned as an array keyed by errors in the top level of a JSON:API document.
+      class JsonApiError < T::Struct
+        # An application-specific error code, expressed as a string value.
+        const :code, Symbol
+    
+        # A human-readable explanation specific to this occurrence of the problem.
+        # Like title, this field's value can be localized.
+        const :detail, T.nilable(String)
+    
+        const :source, T.nilable(JsonApiErrorSource)
+      end
+    end
+end
