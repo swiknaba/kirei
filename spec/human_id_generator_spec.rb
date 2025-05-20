dc26297e4442ce954728285786924df08736dd4b
Lud <lud@reinmiedl.com>
2025-05-20 09:29:04 +0800

    Add HumanIdGenerator spec

---
+++ spec/human_id_generator_spec.rb
@@ -0,0 +1,14 @@
+# typed: false
+
+require 'spec_helper'
+
+RSpec.describe Kirei::Model::HumanIdGenerator do
+  describe '.call' do
+    it 'generates an id with the prefix and desired length' do
+      id = described_class.call(length: 8, prefix: 'abc')
+
+      expect(id).to start_with('abc_')
+      expect(id.length).to eq(12)
+    end
+  end
+end
